added correct puzzle spacing and weighting

scripts/piece_outline_detector.py
@@ -219,6 +219,19 @@ class Detector:
         # Crops unnecessary parts of the image out
         return img[:, 200:, :]
 
+    def find_aruco(self, index):
+        '''
+        Finds an Aruco marker with id index'''
+        image_msg = rospy.wait_for_message("/usb_cam/image_raw", Image)
+        image = self.crop_raw(self.bridge.imgmsg_to_cv2(image_msg, "bgr8"))
+
+        (all_corners, ids, rejected) = cv2.aruco.detectMarkers(image, self.arucoDict, parameters=self.arucoParams)
+        all_corners = np.array(all_corners).reshape((-1,2))
+        ids = ids.flatten()
+        i = ids.index(index)
+        return all_corners[i*4:i*4+4].mean(axis=0)
+
+
     def init_aruco(self):
         image_msg = rospy.wait_for_message("/usb_cam/image_raw", Image)
         image = self.crop_raw(self.bridge.imgmsg_to_cv2(image_msg, "bgr8"))
@@ -232,10 +245,8 @@ class Detector:
         #all_corners = cv2.undistortPoints(np.float32(all_corners), self.camK, self.camD)
         all_corners = np.array(all_corners).reshape((-1,2))
 
-        if len(all_corners) != 16:
-            rospy.loginfo(all_corners)
-            rospy.loginfo(ids)
-            raise RuntimeError("Incorrect number of aruco marker corners:" + str(len(all_corners)))
+        if len(all_corners) != 20:
+            raise RuntimeError("Incorrect number of aruco marker corners:" + str(len(all_corners)) + "\nIds found:" + ids)
 
         #Real Coordinates
         world1 = np.array([-.4325, -.1519])
@@ -247,21 +258,20 @@ class Detector:
         box2 = all_corners[4:8]
         box3 = all_corners[8:12]
         box4 = all_corners[12:16]
+        box5 = all_corners[16:20]
 
         screen1 = np.mean(box1, axis=0)
         screen2 = np.mean(box2, axis=0)
         screen3 = np.mean(box3, axis=0)
         screen4 = np.mean(box4, axis=0)
+        screen5 = np.mean(box5, axis=0)
 
         ids = ids.flatten()
         ids_reorder = np.zeros_like(ids)
-        for i in range(len(ids)):
+        for i in range(4):
             ids_reorder[i] = np.where(ids == i)[0]
-        screens = np.float32([screen1, screen2, screen3, screen4])[ids_reorder]
+        screens = np.float32([screen1, screen2, screen3, screen4, screen5])[ids_reorder]
         worlds = np.float32([world1, world2, world3, world4])
-
-        print(screens)
-        print(worlds)
 
         self.transform = cv2.getPerspectiveTransform(screens, worlds)
 

scripts/puzzle_grid.py
@@ -23,11 +23,12 @@ class PuzzleGrid():
             [[[offset_x + i*spacing_width, offset_y + j*spacing_height]for j in range(height_n)] for i in range(width_n)]
         )
 
-    '''
-        Returns the x, y grid location for the first open spot, or none if there is no
-        open location
-    '''
+    
     def get_open_spot(self):
+        '''
+            Returns the x, y grid location for the first open spot, or none if there is no
+            open location
+        '''
         x, y = get_xy_min(self.occupied)
 
         if (self.occupied[x, y] == 0):
@@ -35,11 +36,12 @@ class PuzzleGrid():
         else:
             return None
 
-    '''
-        Returns the x, y grid location for the first non_oriented spot, or none if there is no
-        open location
-    '''
+    
     def get_unoriented_spot(self):
+        '''
+            Returns the x, y grid location for the first non_oriented spot, or none if there is no
+            open location
+        '''
         x, y = get_xy_min(self.oriented)
 
         if (self.oriented[x, y] == 0):
@@ -54,4 +56,62 @@ class PuzzleGrid():
             min_norm1, (x_min, y_min) = np.min(norm1), get_xy_min(norm1)
             if (min_norm1 < 2*self.spacing):
                 self.occupied[x_min, y_min] = 1
+    
+    def get_neighbor(self, loc):
+        '''
+            Returns neighbor in completed puzzle
+        '''
+        if self.occupied[loc[0]+1, loc[1]] != 0:
+            return [loc[0]+1, loc[1]]
+        if self.occupied[loc[0]-1, loc[1]] != 0:
+            return [loc[0]-1, loc[1]]
+        if self.occupied[loc[0], loc[1]+1] != 0:
+            return [loc[0], loc[1]+1]
+        if self.occupied[loc[0], loc[1]-1] != 0:
+            return [loc[0], loc[1]-1]
+        return None
+
+    def does_mate(self, loc):
+        '''
+            Checks if a grid location mates with the completed puzzle so far
+        '''
+        if self.occupied[loc[0]+1, loc[1]] != 0:
+            return True
+        if self.occupied[loc[0]-1, loc[1]] != 0:
+            return True
+        if self.occupied[loc[0], loc[1]+1] != 0:
+            return True
+        if self.occupied[loc[0], loc[1]-1] != 0:
+            return True
+        return False
+
+    def get_pixel_from_grid(self, loc):
+        '''
+            Returns position in pixel space for a piece on the grid
+        '''
+        pos = [[[ 75,  60],
+                [ 65, 170],
+                [ 75, 280],
+                [ 75, 393]],
+
+                [[212,  58],
+                [199 ,175],
+                [207 ,285],
+                [207 ,394]],
+
+                [[354,  57],
+                [339, 170],
+                [339, 275],
+                [339, 384]],
+
+                [[481,  60],
+                [481, 175],
+                [476, 285],
+                [471, 395]],
+
+                [[613,  60],
+                [603, 170],
+                [608, 280],
+                [603, 392]]]
+        return pos[loc[0]][loc[1]]
         

scripts/solver.py
@@ -12,6 +12,7 @@ from sensor_msgs.msg   import Image
 import numpy as np
 
 from thomas_detector import ThomasDetector, ThomasPuzzlePiece
+from puzzle_grid import PuzzleGrid
 import vision
 
 class SolverTask(enum.Enum):
@@ -38,12 +39,12 @@ class Solver:
         self.tasks.append(SolverTask.SeparatePieces)
         self.tasks.append(SolverTask.GetView)
 
-        # Series of moves to perform
-        self.move_queue = []
+        # Series of actions to perform
+        self.action_queue = []
 
+        self.puzzle_grid = PuzzleGrid()
         self.num_pieces = 20
         self.pieces_cleared = 0
-        self.separated_grid = np.zeros((4, 5))
         self.separated_loc = np.array([220, 100])
         self.separated_spacing = 100
         self.pieces_solved = 0
@@ -103,6 +104,10 @@ class Solver:
 
     def apply_next_action(self, controller):
 
+        if self.action_queue:
+            f = self.action_queue.pop(0)
+            f()
+
         if not self.tasks:
             rospy.loginfo("[Solver] No current tasks, sending Idle command.")
             controller.idle()
@@ -151,8 +156,6 @@ class Solver:
 
             piece_origin = piece.get_center()
             piece_destination = self.find_available_piece_spot(piece, rotation_offset)
-            print(piece_origin, piece_destination)
-            #piece_destination = (300, 300)
             controller.move_piece(piece_origin, piece_destination, turn=rotation_offset, jiggle=False)
             return
 
@@ -182,8 +185,15 @@ class Solver:
                 val = cvt_color(piece.natural_img) #* (piece.thomas_mask.reshape(piece.thomas_mask.shape[0], piece.thomas_mask.shape[1], 1) > 128)
                 coords, rot = self.vision.calculate_xyrot(val)
                 rospy.loginfo(f"Piece Location: {coords}, Rotation: {rot}")
-                piece_destination = (coords[0] * 150 + 620, coords[1] * 150 + 370)
-                controller.move_piece(piece_origin, piece_destination, turn = -rot * np.pi/2, jiggle=True)
+                piece_destination = np.array(self.get_puzzle_region[[0, 1]]) + self.puzzle_grid.get_pixel_from_grid(coords)
+                if self.puzzle_grid.occupied.sum() > 0:
+                    weight_destination = self.puzzle_grid.get_neighbor(coords)
+                    weight_pos_offset = np.array(weight_destination) - np.array(coords)
+                    weight_destination = self.puzzle_grid.get_pixel_from_grid(weight_destination)
+                    controller.move_weight(weight_destination)
+                    self.action_queue.append(lambda: controller.move_piece(piece_origin, piece_destination, turn = -rot * np.pi/2, jiggle=True))
+                else:
+                    controller.move_piece(piece_origin, piece_destination, turn = -rot * np.pi/2, jiggle=False)
                 return
         elif curr_task == SolverTask.SeparateOverlappingPieces:
             raise NotImplementedError()

vision/thomas_detector.py
@@ -194,9 +194,9 @@ class ThomasDetector:
                 piece.set_natural_img(cutout_img)
 
         for piece in pieces:
-            r = int(np.sqrt(piece.area) / 4) + 1
+            r = 500#int(np.sqrt(piece.area) / 4) + 1
             color = piece.get_color()
-            cv2.circle(img, piece.get_location(), r, color, -1)
+            img = cv2.circle(img, piece.get_location(), r, color, -1)
             piece_centers.append(piece.get_location())
 
         #markers[res != 0] = 255